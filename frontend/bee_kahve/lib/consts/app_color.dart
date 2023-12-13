6dd7e1828be204121f7194323b723690a436f389
leminurcelik <celikleminur@gmail.com>
2023-12-13 20:25:13 +0300

    Defines the colors of the application

---
+++ frontend/bee_kahve/lib/consts/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color textColor = Color(0xFF21212F);
+  static const Color yellow = Color(0xFFF1DB11);
+  static const Color lightYellow = Color.fromARGB(102, 241, 219, 17);
+  static const Color darkColor = Color.fromARGB(255, 9, 3, 27);
+  static const Color darkPrimary = Color.fromARGB(255, 94, 75, 236);
+}
