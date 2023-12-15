2538ba63b652e129b6ddfaa693785f5c41d3d108
leminurcelik <celikleminur@gmail.com>
2023-12-15 17:28:24 +0300

    Updates color

--- frontend/bee_kahve/lib/consts/app_color.dart
+++ frontend/bee_kahve/lib/consts/app_color.dart
@@ -5,5 +5,5 @@ class AppColors {
   static const Color yellow = Color(0xFFF1DB11);
   static const Color lightYellow = Color.fromARGB(102, 241, 219, 17);
   static const Color darkColor = Color.fromARGB(255, 9, 3, 27);
-  static const Color darkPrimary = Color.fromARGB(255, 94, 75, 236);
+  static const Color scaffoldBackgroundColor = Color(0xFFFFFFFF);
 }
