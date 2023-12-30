51ddaa9ac88b6e3f47194f014c376e872f4ef2d7
Novruz Amirov <novruz.amirov2004@gmail.com>
2023-12-30 15:45:18 +0300

    Configures correct Routers for URL Changes

--- web-administration/web-application/src/App.js
+++ web-administration/web-application/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import SignIn from './signInPage';
+import SignIn from './modules/signInPage';
+import EmployeeSignUp from './modules/employeeSignUp'; // Adjust the path accordingly
+import AdminPage from './modules/adminPage'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <SignIn/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signInPage" />} />
+        <Route path="/signInPage" element={<SignIn />} />
+        <Route path="/employeeSignUp" element={<EmployeeSignUp />} />
+        <Route path="/adminPage" element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
