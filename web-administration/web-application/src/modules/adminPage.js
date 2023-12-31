b5df003bebc6241e76ede2ae9f01af56c4436902
Novruz Amirov <novruz.amirov2004@gmail.com>
2023-12-31 15:12:09 +0300

    Adds security mechanisms to admin page once

--- web-administration/web-application/src/modules/adminPage.js
+++ web-administration/web-application/src/modules/adminPage.js
@@ -16,7 +16,16 @@ const AdminPage = () => {
   const navigate = useNavigate();
 
 
+  useEffect(() => {
+    // Check if the user has a valid token
+    const token = localStorage.getItem('token');
 
+    if (token === 'b1d632f26e83babf1c80709208e1b6ed01312cc94860c327d82107ff3f073e65e81f902169d4ddfe3f837f8297ea8d80085f0ed1f6fc6ee7a84e0383abadf5ba') {
+      fetchCoffeeList();
+    } else {
+      navigate('/signInPage');
+    }
+  }, []);
 
   const fetchCoffeeList = async () => {
     try {
