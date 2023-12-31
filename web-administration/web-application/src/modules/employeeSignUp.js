4a5928f2867d0ca37948c48eff2765c19a6a9dfb
Novruz Amirov <novruz.amirov2004@gmail.com>
2023-12-31 20:36:41 +0300

    Modifies Access Control stages appropriately for secure access

--- web-administration/web-application/src/modules/employeeSignUp.js
+++ web-administration/web-application/src/modules/employeeSignUp.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import {sha512} from 'crypto-hash';
+import { useNavigate } from 'react-router-dom';
 
 const EmployeeSignUp = () => {
   const [name, setName] = useState('');
@@ -9,19 +11,21 @@ const EmployeeSignUp = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [passwordMatchError, setPasswordMatchError] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Check if password and confirm password match
+  
     if (password !== confirmPassword) {
       setPasswordMatchError(true);
+      setErrorMessage('');
       return;
     }
 
     setLoading(true);
-
-    // Add validation logic if needed
 
     const config = {
       headers: {
@@ -30,15 +34,26 @@ const EmployeeSignUp = () => {
       },
     };
 
-    const body = JSON.stringify({ name, email, password, confirmPassword });
-    console.log(body);
+    const hashedValue = await sha512(password)
+    let admin_id = 1
+    let is_admin = false
+    const body = JSON.stringify({ name, email, hashedValue, admin_id, is_admin });
 
     try {
-      const res = await axios.post('http://51.20.117.162:8000/signup', body, config);
-      console.log(res);
+      const res = await axios.post('http://51.20.117.162:8000/web_register', body, config);
+
+      if (res.data.message === "Success") {
+        setSuccessMessage("Account successfully created!");
+        navigate('/signInPage');
+      } else {
+        setErrorMessage(`Error creating account: ${res.data.message}`);
+        setSuccessMessage('');
+      }
       setLoading(false);
     } catch (err) {
-      console.log(err);
+      console.error(err);
+      setErrorMessage("Failed to create account. Please try again.");
+      setSuccessMessage('');
       setLoading(false);
     }
   };
@@ -83,6 +98,14 @@ const EmployeeSignUp = () => {
 
           {passwordMatchError && (
             <p className="error-message">Password and Confirm Password do not match.</p>
+          )}
+
+          {successMessage && (
+            <p className="success-message">{successMessage}</p>
+          )}
+
+          {errorMessage && (
+            <p className="error-message">{errorMessage}</p>
           )}
 
           <button type="submit" disabled={loading}>
