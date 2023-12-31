63a11c0d29b761d203b8e06fcb2629d95afdb002
AdilMahmudlu <adil.mahmudlu03@gmail.com>
2023-12-31 21:42:46 +0300

    Updates main page and profile page, small fix in cart page

--- frontend/bee_kahve/lib/screens/cart/bottom_checkout.dart
+++ frontend/bee_kahve/lib/screens/cart/bottom_checkout.dart
@@ -1,4 +1,5 @@
 import 'package:bee_kahve/consts/app_color.dart';
+import 'package:bee_kahve/screens/cart/payment.dart';
 import 'package:flutter/material.dart';
 
 class CartBottomSheetWidget extends StatelessWidget {
@@ -8,16 +9,17 @@ class CartBottomSheetWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: const Border(top: BorderSide(
-          width: 1,
-        ),
+        border: const Border(
+          top: BorderSide(
+            width: 1,
+          ),
         ),
         color: Theme.of(context).scaffoldBackgroundColor,
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: SizedBox(
-          height: kBottomNavigationBarHeight+1,
+          height: kBottomNavigationBarHeight + 1,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -26,40 +28,43 @@ class CartBottomSheetWidget extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                        "Total (6 coffee)",
-                        style: TextStyle(
+                      "Total (6 coffee)",
+                      style: TextStyle(
                           fontWeight: FontWeight.normal,
                           color: AppColors.textColor,
                           fontSize: 20,
-                          overflow: TextOverflow.ellipsis
-                        ),
+                          overflow: TextOverflow.ellipsis),
                     ),
                     Text(
-                        "16.00\$",
-                        style: TextStyle(
+                      "16.00\$",
+                      style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: AppColors.yellow,
-                          fontSize: 16
-                        ),
+                          fontSize: 16),
                     ),
                   ],
                 ),
               ),
               ElevatedButton(
-                onPressed: (){},
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const PaymentPage()));
+                },
                 style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                backgroundColor: AppColors.yellow,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  backgroundColor: AppColors.yellow,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
                 ),
-              ),
                 child: const Text(
-                    "Checkout",
-                    style: TextStyle(
-                      color: AppColors.darkColor,
-                    ),
-
+                  "Checkout",
+                  style: TextStyle(
+                    color: AppColors.darkColor,
+                  ),
                 ),
               )
             ],
