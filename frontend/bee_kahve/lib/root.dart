1cdf7be3d7027a6093f4fb858542414fb8da2819
AdilMahmudlu <adil.mahmudlu03@gmail.com>
2024-01-01 22:48:16 +0300

    Adds currentScreen parameter for initializing

--- frontend/bee_kahve/lib/root.dart
+++ frontend/bee_kahve/lib/root.dart
@@ -4,22 +4,25 @@ import 'package:bee_kahve/screens/home.dart';
 import 'package:bee_kahve/screens/profile/profile.dart';
 import 'package:flutter/material.dart';
 
-
 class RootScreen extends StatefulWidget {
-  const RootScreen({super.key});
+  final int currentScreen; // Add currentScreen parameter
+
+  const RootScreen({Key? key, required this.currentScreen}) : super(key: key);
 
   @override
   State<RootScreen> createState() => _RootScreenState();
 }
 
-
 class _RootScreenState extends State<RootScreen> {
   late List<Widget> screens;
-  int currentScreen = 0;
+  late int currentScreen; // Remove int currentScreen = 0;
   late PageController controller;
+
   @override
   void initState() {
     super.initState();
+    currentScreen =
+        widget.currentScreen; // Update currentScreen from widget parameter
     screens = const [
       HomeScreen(),
       CartScreen(),
@@ -51,18 +54,42 @@ class _RootScreenState extends State<RootScreen> {
         },
         destinations: const [
           NavigationDestination(
-            selectedIcon: Icon(Icons.home, size: 32, color: AppColors.yellow,),
-            icon: Icon(Icons.home, size: 28, color: AppColors.textColor,),
+            selectedIcon: Icon(
+              Icons.home,
+              size: 32,
+              color: AppColors.yellow,
+            ),
+            icon: Icon(
+              Icons.home,
+              size: 28,
+              color: AppColors.textColor,
+            ),
             label: "Home",
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.shopping_cart, size: 32, color: AppColors.yellow,),
-            icon: Icon(Icons.shopping_cart, size: 28, color: AppColors.textColor,),
+            selectedIcon: Icon(
+              Icons.shopping_cart,
+              size: 32,
+              color: AppColors.yellow,
+            ),
+            icon: Icon(
+              Icons.shopping_cart,
+              size: 28,
+              color: AppColors.textColor,
+            ),
             label: "Cart",
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.account_circle, size: 32, color: AppColors.yellow,),
-            icon: Icon(Icons.account_circle, size: 28, color: AppColors.textColor,),
+            selectedIcon: Icon(
+              Icons.account_circle,
+              size: 32,
+              color: AppColors.yellow,
+            ),
+            icon: Icon(
+              Icons.account_circle,
+              size: 28,
+              color: AppColors.textColor,
+            ),
             label: "Profile",
           ),
         ],
