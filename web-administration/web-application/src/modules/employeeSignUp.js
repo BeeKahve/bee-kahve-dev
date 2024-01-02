a4358de624655464ebb2f3a60c1b33e9f04e5e86
Novruz Amirov <novruz.amirov2004@gmail.com>
2024-01-02 17:59:23 +0300

    Handles Issue#3 Part 1

--- web-administration/web-application/src/modules/employeeSignUp.js
+++ web-administration/web-application/src/modules/employeeSignUp.js
@@ -21,6 +21,7 @@ const EmployeeSignUp = () => {
   const [passwordMatchError, setPasswordMatchError] = useState(false);
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [registerPass, setRegisterPass] = useState('');
 
   const navigate = useNavigate();
 
@@ -47,6 +48,13 @@ const EmployeeSignUp = () => {
     const body = JSON.stringify({ name, email, hashedValue, admin_id, is_admin });
 
     try {
+      const hashedRegister = await sha512(registerPass);
+      if(hashedRegister !== '1cbc282b219ce3a9f2e5b9ee9cc25648cfcc078e39b8ac43dc0041e56389a64ddb0ec51ad6c5920e565aec27403a1b27b7fd93b9c298c4b54ab98475e4209d67'){
+        setErrorMessage('Register password is incorrect. Registration without it is not possible');
+        return;
+      }
+
+    
       setLoading(true);
       const res = await axios.post('http://51.20.117.162:8000/web_register', body, config);
 
@@ -107,6 +115,14 @@ const EmployeeSignUp = () => {
             placeholder="Confirm Password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            name="registerPass"
+            placeholder="Please enter a register password to create account"
+            value={registerPass}
+            onChange={(e) => setRegisterPass(e.target.value)}
             required
           />
 
