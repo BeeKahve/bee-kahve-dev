a0ce06de915b20abf52be90f02bcf63304ac50df
Novruz Amirov <novruz.amirov2004@gmail.com>
2024-01-02 13:51:58 +0300

    Configures appropriate API calls for active orders

--- web-administration/web-application/src/modules/updateStatus.js
+++ web-administration/web-application/src/modules/updateStatus.js
@@ -1,5 +1,5 @@
 // updateStatus.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Button, Table, Modal, Form } from 'antd';
 import axios from 'axios';
 import { useNavigate , useLocation} from 'react-router-dom';
@@ -13,30 +13,38 @@ const UpdateStatus = () => {
   const [selectedOrder, setSelectedOrder] = useState({});
   const location = useLocation();
   const employeeName = location.state && location.state.employeeName;
+  const navigate = useNavigate()
 
-
-  const navigate = useNavigate();
-
-//   useEffect(() => {
-//     // Check if the user has a valid token or role for employee
-//     const token = localStorage.getItem('token');
-//     const role = localStorage.getItem('role');
-
-//     if (token && role === 'employee') {
-//       fetchOrderList();
-//     } else {
-//       navigate('/signInPage'); // Redirect to sign-in if not authenticated as employee
-//     }
-//   }, []);
-
-  const fetchOrderList = async () => {
+  const fetchOrderList = useCallback(async () => {
     try {
-      const response = await axios.get('/api/orders');
-      setOrderList(response.data);
+      const response = await axios.post('http://51.20.117.162:8000/get_active_orders?admin_id=1');
+      console.log(response)
+      setOrderList(response.data.orders);
     } catch (error) {
       console.error('Error fetching order list:', error);
     }
-  };
+  }, [setOrderList]);
+
+  useEffect(() => {
+    // Check if the user has a valid token or role for employee
+    const token = localStorage.getItem('token');
+    const name = localStorage.getItem('name');
+
+    try{
+      if(token === "b1d632f26e83babf1c80709208e1b6ed01312cc94860c327d82107ff3f073e65e81f902169d4ddfe3f837f8297ea8d80085f0ed1f6fc6ee7a84e0383abadf5ba"){
+        navigate('/adminPage')
+      }
+      if (token && name === employeeName) {
+        fetchOrderList();
+      } else {
+        navigate('/signInPage'); // Redirect to sign-in if not authenticated as employee
+      }
+    }
+    catch{
+      navigate('/signInPage'); // Redirect to sign-in if not authenticated as employee
+    }
+  }, [fetchOrderList, navigate, employeeName]);
+
 
   const showModal = (title, order) => {
     setIsModalVisible(true);
@@ -95,13 +103,13 @@ const UpdateStatus = () => {
           <h2>Order List</h2>
           {/* Table for Order List */}
           <Table dataSource={orderList} rowKey="_id">
-            <Column title="Order ID" dataIndex="_id" key="_id" render={(text, record) => <h3>Order ID: {text}</h3>} />
-            <Column title="Products" dataIndex="products" key="products" render={(text, record) => (
+            <Column title="Order ID" dataIndex="order_id" key="order_id" render={(text, record) => <h3>Order ID: {text}</h3>} />
+            <Column title="Products" dataIndex="line_items" key="line_items" render={(text, record) => (
               <div>
                 {/* Render product details for each order */}
                 {text.map((product) => (
-                  <div key={product._id}>
-                    <img src={product.image} alt={product.name} style={{ width: 50, height: 50, marginRight: 8 }} />
+                  <div key={product.product_id}>
+                    <img src={product.photo_path} alt={product.name} style={{ width: 100, height: 100, marginRight: 8 }} />
                     <span>{product.name} - Size: {product.size} - Price: {product.price}</span>
                   </div>
                 ))}
