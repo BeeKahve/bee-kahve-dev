463b408f0f309379eb916f9aaadb49ec79e015d5
AdilMahmudlu <adil.mahmudlu03@gmail.com>
2024-01-04 23:23:03 +0300

    adds function to turn list to map

--- frontend/bee_kahve/lib/models/past_order_model.dart
+++ frontend/bee_kahve/lib/models/past_order_model.dart
@@ -13,4 +13,16 @@ class Order {
       required this.lineItems,
       this.orderDate,
       this.orderStatus});
+
+  Map<Coffee, int> listToMap() {
+    Map<Coffee, int> productWithCounts = {};
+    for (Coffee item in lineItems) {
+      if (productWithCounts.containsKey(item)) {
+        productWithCounts.update(item, (value) => value + 1);
+      } else {
+        productWithCounts.putIfAbsent(item, () => 1);
+      }
+    }
+    return productWithCounts;
+  }
 }
