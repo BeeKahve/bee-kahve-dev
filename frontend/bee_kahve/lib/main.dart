7f9558ae202526412570fba4d05dc87459e7a23f
leminurcelik <celikleminur@gmail.com>
2024-01-05 12:44:18 +0300

    Remove debug writing show

--- frontend/bee_kahve/lib/main.dart
+++ frontend/bee_kahve/lib/main.dart
@@ -8,13 +8,13 @@ void main() {
     ChangeNotifierProvider(
       create: (context) => CartProvider(),
       child: const MyApp(),
+      
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -39,6 +39,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const SignInScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
